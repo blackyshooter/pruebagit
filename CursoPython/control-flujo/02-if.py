380fece715e1332d6ae82341cae8995addd18ea7
blackyshooter <elias.soto.py@outlook.com>
2024-02-06 10:27:08 -0300

    Curso Holamundo

---
+++ CursoPython/control-flujo/02-if.py
@@ -0,0 +1,11 @@
+edad = 15
+
+
+if edad > 54:
+    print("Tiene un descuento para la pelicula")
+elif edad > 17:
+    print("Puedes ver la peliula")
+
+# else:
+  #  print("Acceso Denegado debe ser mayor de edad")
+  #  print("Vete a otro lado")
