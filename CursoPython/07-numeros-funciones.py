3da89b43d4d07fb1d00fb7e76f7cdac0128aebdb
blackyshooter <elias.soto.py@outlook.com>
2024-02-06 09:51:43 -0300

    Curso Holamundo

---
+++ CursoPython/07-numeros-funciones.py
@@ -0,0 +1,16 @@
+import math
+
+print(round(1.5))
+
+print(abs(-2))
+
+# Metodos mas importantes de "Math"#
+
+print(math.ceil(1.1))  # Entero mas cercano hacia arriba#
+
+print(math.floor(1.9999))  # Entero mas cercano hacia abajo#
+print(math.isnan(23))
+# print(math.isnan("23")) #Indica si el numero es un numero real (boolean)
+
+print(math.pow(10, 3))
+print(math.sqrt(9))
