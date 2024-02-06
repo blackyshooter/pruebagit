7bdd53c6cac9ae0c71cb25d466219c3020982f8b
blackyshooter <elias.soto.py@outlook.com>
2024-02-05 22:28:28 -0300

    Curso Holamundo

--- CursoPython/04-metodos-strings.py
+++ CursoPython/04-metodos-strings.py
@@ -1,7 +1,13 @@
-animal = "  chanchito feliz  "
+animal = "  chanCHito feliz  "
 
 print(animal.upper())
 print(animal.lower())
 print(animal.strip().capitalize())
 print(animal.title())
 print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("CH"))
+print(animal.replace("CH","ch"))
+print("nCH" in animal)
+print("nCH" not in animal)
