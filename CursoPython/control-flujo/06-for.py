168d29cabb2f56bc99e9fa22b59702598068d827
blackyshooter <elias.soto.py@outlook.com>
2024-02-07 16:12:59 -0300

    Curso Holamundo

--- CursoPython/control-flujo/06-for.py
+++ CursoPython/control-flujo/06-for.py
@@ -1,10 +1,13 @@
 
 buscar = 10
 
-for numero in range(5):
+for numero in range(5):  # Iterable
     print(numero)
     if numero == buscar:
         print("Aca estoy rey", buscar)
         break
 else:
     print("No esta :(")
+
+for char in "Ultimate Python":
+    print(char)
