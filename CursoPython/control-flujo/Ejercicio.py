2519beb2082f8a3085e12cea98758a81a242b8a3
blackyshooter <elias.soto.py@outlook.com>
2024-02-07 22:04:05 -0300

    Curso Holamundo

--- CursoPython/control-flujo/Ejercicio.py
+++ CursoPython/control-flujo/Ejercicio.py
@@ -1,34 +1,31 @@
 
-n1 = 0
+resultado = ""
 
-if n1 == 0:
-        
-    leer_n1 = input("Ingrese un número ")
-    n1 = int(leer_n1)
 while True:
+    if not resultado:
+        leer_n1 = input("Ingrese un número ")
+    if leer_n1.lower == "salir":
+        break
+    resultado = int(leer_n1)
     leer_op = input("\nIngrese la operacion ")
     if leer_op.lower() == "salir":
         break
     leer_n2 = input("\nIngrese el siguiente numero ")
     n2 = int(leer_n2)
     if leer_op.lower() == "suma":
-        resultado = n1 + n2
-        print("El resultado es ",resultado)
+        resultado += n2
         n1 = resultado
     elif leer_op.lower() == "resta":
-        resultado = n1 - n2
-        print("El resultado es ",resultado)
+        resultado -= n2
         n1 = resultado
     elif leer_op.lower() == "multi":
-        resultado = n1 * n2
-        print("El resultado es ",resultado)
+        resultado *= n2
         n1 = resultado
     elif leer_op.lower() == "div":
-        resultado = n1 / n2
-        print("El resultado es ",resultado)
+        resultado /= n2
         n1 = resultado
-   
+    else:
+        print("Operacion Invalida")
+        break
 
-    
-
- 
+    print(f"El resultado es {resultado}")
