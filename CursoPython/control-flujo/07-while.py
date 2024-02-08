6e72181e300e1745536c5e5f9adf5233202e893b
blackyshooter <elias.soto.py@outlook.com>
2024-02-07 21:26:02 -0300

    Curso Holamundo

--- CursoPython/control-flujo/07-while.py
+++ CursoPython/control-flujo/07-while.py
@@ -1,5 +1,4 @@
 # numero = 1
-<<<<<<< HEAD
 
 # while numero < 100:
 #     print(numero)
@@ -10,17 +9,3 @@ comando = ""
 while comando.lower() != "salir":
     comando = input("$")
     print(comando)
-=======
-# while numero < 100
-#       print(numero)
-#       numero*=2
-
-comando = ""
-
-while True:
-    comando = input("$")
-    print(comando)
-    if comando.lower == "salir":
-        break
-
->>>>>>> 4090b471211210daec6fab85cb1362affa47bb15
