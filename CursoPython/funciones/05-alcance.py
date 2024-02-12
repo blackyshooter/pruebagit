d6983ff4327a6fd219f01e92e4dce82d1697bdca
blackyshooter <elias.soto.py@outlook.com>
2024-02-11 22:16:41 -0300

    Curso Holamundo

---
+++ CursoPython/funciones/05-alcance.py
@@ -0,0 +1,19 @@
+saludo = 25
+
+
+def saludar():
+    global saludo
+    saludo = "Hola mundo"
+
+
+def saludaChanchito():
+    saludo = "Hola Chanchito"
+    print(saludo)
+
+
+resultado1 = saludo + 3
+print(resultado1)
+saludar()
+resultado2 = saludo + 3
+
+print(resultado2)
