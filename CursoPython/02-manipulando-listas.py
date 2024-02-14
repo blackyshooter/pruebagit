fa67deb92526f90f0a0d4f519f0abfb8abd1629e
blackyshooter <elias.soto.py@outlook.com>
2024-02-13 21:45:44 -0300

    Curso Holamundo

---
+++ CursoPython/02-manipulando-listas.py
@@ -0,0 +1,9 @@
+mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
+mascotas[0] = "Bicho"
+
+# print(mascotas[1:2:2])
+
+
+numeros = list(range(21))
+print(numeros[::2])
+print(numeros[1::2])
---
+++ CursoPython/tipos-avanzados/01-listas.py
@@ -0,0 +1,14 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+palabrasFelices = ["chanchito", "feliz", "Felipe", "alumno"]
+booleans = [True, False, True, True]
+matriz = [[0, 1], [1, 0]]
+ceros = [0, 1] * 10
+alfanumerico = numeros + letras
+print(alfanumerico)
+rango = list(range(1, 11))
+
+chars = list("hola mundo")
+
+print(chars)
